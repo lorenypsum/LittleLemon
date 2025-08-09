f33fa70883ea000fc35f7bba979cb2cd44d6f846
lorenypsum <loren.ypsum@gmail.com>
2025-08-08 22:39:11 -0300

    a

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('menu/', views.menu, name='menu'),
-    path('menu', views.MenuList.as_view()),
-    path('menu/<int:pk>/', views.Menu.as_view()),
+    # path('menu', views.MenuList.as_view()),
+    path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -7,8 +7,6 @@ from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
-
-
 # Create your views here.
 @api_view(['GET', 'POST'])
 def menu(request):
@@ -18,16 +16,20 @@ class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
-class MenuList(APIView):
-    def get(self, request):
-        return Response({"message": "List of menu items"}, status=status.HTTP_200_OK)
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
-    def post(self, request):
-        return Response({"message": "Menu item created"}, status=status.HTTP_201_CREATED)
+# class MenuList(APIView):
+#     def get(self, request):
+#         return Response({"message": "List of menu items"}, status=status.HTTP_200_OK)
 
-class Menu(APIView):
-    def get(self, request, pk):
-        return Response({"message": f"Details of menu item {pk}"}, status=status.HTTP_200_OK)
+#     def post(self, request):
+#         return Response({"message": "Menu item created"}, status=status.HTTP_201_CREATED)
+
+# class Menu(APIView):
+#     def get(self, request, pk):
+#         return Response({"message": f"Details of menu item {pk}"}, status=status.HTTP_200_OK)
     
-    def put(self, request, pk):
-        return Response({"title": request.data.get("title", "")}, status=status.HTTP_200_OK)
+#     def put(self, request, pk):
+#         return Response({"title": request.data.get("title", "")}, status=status.HTTP_200_OK)
